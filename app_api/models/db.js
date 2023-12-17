9d3f23150d30bff97f4b1c75e342fbbb23675eb7
Furkan <114299899+FurcanY@users.noreply.github.com>
2023-12-17 15:32:34 +0300

    init

---
+++ app_api/models/db.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var dbURI='mongodb://localhost/mekanbul';
+mongoose.connect(dbURI);
+mongoose.connection.on("connected",function(){
+    console.log(dbURI+" adresine bağlandı");
+});
+mongoose.connection.on("error",function(){
+    console.log("bağlantıda hata oldu");
+});
+mongoose.connection.on("disconnected",function(){
+    console.log("Bağlantı kesildi");
+});
+process.on("SIGINT",function(){
+    mongoose.connection.console;
+    console.log("Uygulama Kapatıldı");
+    process.exit(0);
+});
+
+require("./venue");
